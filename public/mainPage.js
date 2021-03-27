cb3dad9d9a8b0d3266f5cebe302ef080929c7796
kutaycosar <kutayc@gmail.com>
2021-03-28 00:12:10 +0300

    tercume buttonlari halloldu

--- public/mainPage.js
+++ public/mainPage.js
@@ -34,6 +34,8 @@ function alertTimeoutFail(mymsg,mymsecs)
  document.body.appendChild(myelement);
 }
 
+
+
 document.getElementById("create-form").addEventListener("submit", function(e){
   validation()
   console.log(errors.length)
